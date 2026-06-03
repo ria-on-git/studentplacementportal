eefcaacd58bf86a0334570d24e5101bb00a72e6d
ria-on-git <riakateimp@gmail.com>
2026-06-03 18:15:10 +0530

    Refactor MySQL connection handling
    Updated database connection logic to handle missing MYSQL_URL gracefully.

--- db.js
+++ db.js
@@ -1,24 +1,24 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-console.log('MYSQL_URL exists:', !!process.env.MYSQL_URL);
-console.log('MYSQL_URL value:', process.env.MYSQL_URL ? process.env.MYSQL_URL.substring(0, 30) + '...' : 'NOT FOUND');
-
 const connectionUrl = process.env.MYSQL_URL;
 
 if (!connectionUrl) {
-  console.error('ERROR: MYSQL_URL is missing!');
-  process.exit(1);
+  console.error('WARNING: MYSQL_URL is not set!');
 }
 
-const db = mysql.createConnection(connectionUrl);
+const db = connectionUrl ? mysql.createConnection(connectionUrl) : null;
 
-db.connect(function(err) {
-  if (err) {
-    console.log('DB connection failed:', err.message);
-  } else {
-    console.log('Connected to MySQL!');
-  }
-});
+if (db) {
+  db.connect(function(err) {
+    if (err) {
+      console.error('DB connection failed:', err.message);
+    } else {
+      console.log('Connected to MySQL!');
+    }
+  });
+} else {
+  console.log('No database connection - running without DB');
+}
 
 module.exports = db;
