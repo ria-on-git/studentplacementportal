57e1318d61a49b3efc6f7b72a4484bdfda077f8f
ria-on-git <riakateimp@gmail.com>
2026-06-03 11:47:48 +0530

    Refactor database connection to use MYSQL_URL

--- db.js
+++ db.js
@@ -1,12 +1,7 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-  host:     process.env.DB_HOST,
-  user:     process.env.DB_USER,      
-  password: process.env.DB_PASSWORD,          
-  database: process.env.DB_NAME  
-});
+const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect(function(err) {
   if (err) {
